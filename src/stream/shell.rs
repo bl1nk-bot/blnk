f13a98e5e86776e358b10a6d8eab945c2de3ff64
billlzzz18 <2.1660379e+08+billlzzz18@users.noreply.github.com>
2026-08-21 12:26:25 +0000

    test(windows): use cmd-safe shell fixtures

--- src/stream/shell.rs
+++ src/stream/shell.rs
@@ -401,7 +401,7 @@ mod tests {
     fn capture_script() -> &'static str {
         #[cfg(windows)]
         {
-            "echo(out&echo(err 1>&2&exit /b 7"
+            "echo(out&echo(err>&2&exit /b 7"
         }
         #[cfg(not(windows))]
         {
@@ -412,7 +412,7 @@ mod tests {
     fn long_running_script() -> &'static str {
         #[cfg(windows)]
         {
-            "timeout /T 1 /NOBREAK >NUL 2>&1"
+            "for /L %i in (1,1,2147483647) do @rem"
         }
         #[cfg(not(windows))]
         {
